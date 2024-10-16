44750771cd9976e2baba8c7ae80fc0ef06ce91fe
Gilianne Kate Alivia Reyes <giliannekatereyes@yahoo.com>
2024-10-16 20:30:33 +0200

    Modify Cookbook class

--- src/main/java/edu/ntnu/iir/bidata/Cookbook.java
+++ src/main/java/edu/ntnu/iir/bidata/Cookbook.java
@@ -1,7 +1,6 @@
 package edu.ntnu.iir.bidata;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 /**
  * Represents a cookbook with a collection of recipes.
@@ -9,7 +8,7 @@ import java.util.Optional;
  * Cookbook has the following functionalities:
  * <ul>
  *   <li> Add a recipe</li>
- *   <li> Display available recipes</li>
+ *   <li> Display recipes that can be made with the ingredients in the fridge</li>
  *   <li> Retrieve a recipe by name</li>
  * </ul>
  */
@@ -33,27 +32,50 @@ public class Cookbook {
   }
 
   /**
-   * Displays all available recipes that can be made with the ingredients in the fridge.
+   * Displays all recipes that can be made with the ingredients in the fridge.
+   * Details of each recipe is displayed on the console.
    *
    * @param fridge The fridge containing the ingredients to check against recipes.
    */
   public void showAvailableRecipes(Fridge fridge) {
-    recipes.stream()
-        .filter(recipe -> recipe.available(fridge)) // Filters available recipes
-        .forEach(Recipe::showRecipe); // Displays each available recipe
+    if (recipes.isEmpty()) {
+      System.out.println("There are no recipes available.");
+    } else {
+      recipes.stream()
+          .filter(recipe -> recipe.hasRequiredIngredients(fridge)) // Filters recipes that can be made
+          .forEach(Recipe::showRecipe); // Displays details of each recipe
+    }
   }
 
   /**
-   * Retrieves a recipe by its name.
+   * Retrieves a recipe from the cookbook by its name.
    *
    * @param name The name of the recipe to retrieve.
    *
-   * @return An Optional containing the recipe if found, or an empty Optional if not found.
+   * @return The recipe to retrieve.
+   *
+   * @throws IllegalArgumentException if a recipe with the provided name does not exist.
    */
-  public Optional<Recipe> getRecipeByName(String name) {
+  public Recipe findRecipeByName(String name) {
     return recipes.stream()
         .filter(recipe -> recipe.getName().equalsIgnoreCase(name))
-        .findAny();
+        .findAny()
+        .orElseThrow(() -> new IllegalArgumentException("Recipe not found: " + name));
+  }
+
+  /**
+   * Checks if the required ingredients for a recipe in the cookbook
+   * is available in the fridge.
+   *
+   * @param fridge The fridge to check for ingredients.
+   * @param recipeName The name of the recipe to check.
+   *
+   * @return {@code true} if the recipe has all the required ingredients available.
+   *         Otherwise, it returns {@code false}.
+   */
+  public boolean canMakeRecipe(Fridge fridge, String recipeName) {
+    Recipe recipe = findRecipeByName(recipeName);
+    return recipe.hasRequiredIngredients(fridge);
   }
 
 }
