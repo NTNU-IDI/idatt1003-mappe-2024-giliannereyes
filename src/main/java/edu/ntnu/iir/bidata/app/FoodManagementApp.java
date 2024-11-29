1c8d867a9fbeeb87e0095099bbcd7989440833cb
Gilianne Kate Alivia Reyes <giliannekatereyes@yahoo.com>
2024-11-29 02:42:33 +0100

    refactor(foodManagementApp): update main method to accommodate changes in init logic, add javaDocs

--- src/main/java/edu/ntnu/iir/bidata/app/FoodManagementApp.java
+++ src/main/java/edu/ntnu/iir/bidata/app/FoodManagementApp.java
@@ -1,23 +1,18 @@
 package edu.ntnu.iir.bidata.app;
 
-import edu.ntnu.iir.bidata.utils.Manager;
-import edu.ntnu.iir.bidata.model.MealPlanner;
-import edu.ntnu.iir.bidata.model.Cookbook;
-import edu.ntnu.iir.bidata.model.Fridge;
-import edu.ntnu.iir.bidata.ui.InputHandler;
 import edu.ntnu.iir.bidata.ui.Ui;
 
+/**
+ * Wrapper class for the Food Management Application.
+ */
 public class FoodManagementApp {
+  /**
+   * Main method to start the application.
+   *
+   * @param args are the arguments for the main method.
+   */
   public static void main(String[] args) {
-    // Create instances of the classes needed to run the application
-    Fridge fridge = new Fridge();
-    Cookbook cookbook = new Cookbook();
-    MealPlanner mealPlanner = new MealPlanner(fridge, cookbook);
-    InputHandler inputHandler = new InputHandler();
-    Manager manager = new Manager(fridge, cookbook, mealPlanner);
-
-    // Start the application by creating a new Ui instance and calling the start method
-    Ui ui = new Ui(inputHandler, manager);
+    Ui ui = new Ui();
     ui.start();
   }
 }
